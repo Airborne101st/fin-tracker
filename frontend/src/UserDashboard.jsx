e8e3ec8e4b00d13579678df48dd1494019ad8eae
akshat <akshatsharma101st@gmail.com>
2025-01-04 12:16:14 +0530

    [ADD] Added Dropdown in dashboard

--- frontend/src/UserDashboard.jsx
+++ frontend/src/UserDashboard.jsx
@@ -1,58 +1,94 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Bar, Doughnut } from "react-chartjs-2";
-import { Chart, ArcElement, Tooltip, Legend, CategoryScale, BarElement, Title, LinearScale } from "chart.js";
-import axios from "axios"
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import {
+  Chart,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  BarElement,
+  Title,
+  LinearScale,
+} from "chart.js";
+import axios from "axios";
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import NavigationBar from "./navbar";
 import Footer from "./footer";
 import AddCategory from "./AddCategory";
-Chart.register(ArcElement, Tooltip, Legend, LinearScale, CategoryScale, BarElement, Title);
+Chart.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  Title
+);
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_SERVICE, // Assuming your backend API base URL
 });
 
+const Dropdown = ({ selectedOption, handleSelectChange }) => {
+  return (
+    <Form.Group>
+      <Form.Label>Filter</Form.Label>
+      <Form.Select value={selectedOption} onChange={handleSelectChange}>
+        <option value="all">All</option>
+        <option value="last_week">Last Week</option>
+        <option value="last_month">Last Month</option>
+        <option value="last_6_months">Last 6 Months</option>
+        <option value="last_year">Last Year</option>
+      </Form.Select>
+    </Form.Group>
+  );
+};
+
 const UserDashboard = () => {
   const navigate = useNavigate();
   const [data, setData] = useState(null);
+  const [selectedOption, setSelectedOption] = useState("all");
+
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       const userId = localStorage.getItem("user_id");
-      const token = localStorage.getItem("token")
+      const token = localStorage.getItem("token");
 
       try {
-        const response = await apiClient.get(`/expenses/expense-summary/user/${userId}`);
+        const response = await apiClient.get(
+          `/expenses/expense-summary/user/${userId}/filter/${selectedOption}`
+        );
 
         console.log(response.data);
-        
 
         const categories = response.data.map((item) => item.category);
         const totals = response.data.map((item) => item.total);
 
         const options = {
-                          plugins: {
-                              title: {
-                                  display: true,
-                                  text: 'Doughnut Chart',
-                                  color:'blue',
-                                  font: {
-                                      size:34
-                                  },
-                                  padding:{
-                                      top:30,
-                                      bottom:30
-                                  },
-                                  responsive:true,
-                                  animation:{
-                                      animateScale: true,
-                                                }
-                              }
-                          }
-      
-                      };
-
+          plugins: {
+            title: {
+              display: true,
+              text: "Doughnut Chart",
+              color: "blue",
+              font: {
+                size: 34,
+              },
+              padding: {
+                top: 30,
+                bottom: 30,
+              },
+              responsive: true,
+              animation: {
+                animateScale: true,
+              },
+            },
+          },
+        };
 
         setData({
           labels: categories,
@@ -85,12 +121,9 @@ const UserDashboard = () => {
     };
 
     fetchData();
-  }, []);
-
+  }, [selectedOption]);
 
   if (!data) return <div>Loading...</div>;
-
-
 
   return (
     <>
@@ -101,16 +134,22 @@ const UserDashboard = () => {
       <Container className="mt-5" fluid>
         <Row>
           <Col md={4}>
-        <div className="chart-container">  
-        <Doughnut data={data}/>
-        </div>
-        </Col>
-        <Col md={8}>
-          <Bar data={data}/>
-        </Col>
+            <div className="chart-container">
+              <Doughnut data={data} />
+            </div>
+
+            <div>
+              <Dropdown
+                selectedOption={selectedOption}
+                handleSelectChange={handleSelectChange}
+              />
+            </div>
+          </Col>
+          <Col md={8}>
+            <Bar data={data} />
+          </Col>
         </Row>
-        <Row>
-        </Row>
+        <Row></Row>
       </Container>
 
       {/* Footer */}
